53a513808d7a1a75dfc0b99a5dc4ec9eec0ae554
Dimitris Verginis <d.verginis777@gmail.com>
2022-04-19 19:04:51 +0300

    progress-on-hourly-data

--- weatherApp/controller.py
+++ weatherApp/controller.py
@@ -1,14 +1,53 @@
-import requests
 import json
+from pprint import pprint
+
+import requests
+
 
 class Controller:
 
-    def __init__(self, api_key, default_call, units):
-        self.API_KEY = api_key
+    def __init__(self, api_key, default_call, units, language=None):
+        self._API_KEY = api_key
         self.defaultCall = default_call
         self.units = units
+        self.language = language
+    
+    def __getAPIKEY(self):
+        return self._API_KEY
+
+    def getDefaultCall(self):
+        return self.defaultCall
+
+    def getUnits(self):
+        return self.units
+
+    def getLanguage(self):
+        return self.language
     
     def getWeatherCity(self, cityId):
         print("WeEaTheR")
+    
+    def getHourlyData(self, latitude, longitude):
+        if not isinstance(longitude, float):
+            raise TypeError(f"Longitude but be of type float.{type(longitude)} given")
+        if not isinstance(latitude, float):
+            raise TypeError(f"Latitude but be of type float.{type(latitude)} given")
 
+        apiCALL = self.getDefaultCall() + \
+            f"onecall?lat={latitude}&lon={longitude}&" + \
+                f"exclude=current,minutely,daily,alerts&units={self.getUnits()}&appid={self.__getAPIKEY()}"
+        data = requests.get(apiCALL)
 
+        if data.status_code == 400:
+            raise BadRequest(data.reason)
+            
+        return data 
+
+class BadRequest(Exception):
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return f"{self.message}. Not acceptable longitude or latitude."
+        
--- weatherApp/mainApp.py
+++ weatherApp/mainApp.py
@@ -4,6 +4,9 @@ from controller import Controller
 
 API_KEY = "40532d33f39f395622bd004abeb82179"
 default_call = "https://api.openweathermap.org/data/2.5/"
+units = "metric"
 
-app = Controller(1234,12314)
-app.getWeatherCity(123)
+app = Controller(API_KEY,default_call, units)
+lon =23.727539
+lat =  317.983810 
+app.getHourlyData(lat, lon)
