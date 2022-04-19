90276c1e1dfd36fb928a68ba29e160dc23238ec0
Dimitris Verginis <d.verginis777@gmail.com>
2022-04-19 16:48:30 +0300

    sorry tha ksananevaso to venv

--- weatherApp/controller.py
+++ weatherApp/controller.py
@@ -3,8 +3,9 @@ import json
 
 class Controller:
 
-    def __init__(self, api_key, units):
+    def __init__(self, api_key, default_call, units):
         self.API_KEY = api_key
+        self.defaultCall = default_call
         self.units = units
     
     def getWeatherCity(self, cityId):
