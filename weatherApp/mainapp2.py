b0b4be20fd39b766811a4effc2a2de1ea1b31e44
Jopapadam <jopapadam@hotmail.co.uk>
2022-04-20 12:03:11 +0300

    smol changes

--- weatherApp/mainapp2.py
+++ weatherApp/mainapp2.py
@@ -4,6 +4,5 @@ from controller import Controller
 
 API_KEY = "40532d33f39f395622bd004abeb82179"
 default_call = "https://api.openweathermap.org/data/2.5/"
-
-app = Controller(1234,12314)
-app.getWeatherCity(123)
+units = "metric"
+app = WeatherController(API_KEY,default_call, units)
