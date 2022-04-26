a4eda7350eac6a87020d486bff29fb31ccd9642c
Dimitris Verginis <d.verginis777@gmail.com>
2022-04-26 17:21:10 +0300

    begun-gui-development

--- weatherApp/mainApp.py
+++ weatherApp/mainApp.py
@@ -1,26 +1,24 @@
 
-from pprint import pprint
-from weatherController import  WeatherController
-import json
-import datetime 
-from matplotlib import pyplot as plt
-from dataManager import DataManager as dm
-
+import PyQt5.QtWidgets as qtw
+import PyQt5.QtCore as qtc
+import PyQt5.QtGui as qtg
+from maingui import Ui_Form
 API_KEY = "40532d33f39f395622bd004abeb82179"
 geoKEY = "f7abd3f018df76b5c983da7768b7cf2a"
 default_call = "https://api.openweathermap.org/data/2.5/"
 units = "metric"
 
+class LoginGui(qtw.QWidget):
 
-app = WeatherController(API_KEY,units)
+    def __init__(self):
+        super().__init__()
+        self.loginUI = Ui_Form()
+        self.loginUI.setupUi(self)
+        self.show()
 
-city = "Athens"
-data = app.getHourlyData(city)
 
-temps = dm.returnTemperatures(data, 1)
-test = dm.returnData(data, "uvi")
-print(test)
-timestamps = dm.returnTimestamps(data, "hour")
 
-plt.plot(timestamps,temps)
-plt.show()
+if __name__ == '__main__':
+    app = qtw.QApplication([])
+    mw = LoginGui()
+    app.exec_()
