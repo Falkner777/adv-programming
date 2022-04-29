83e5db99828fbbdcf2f7042a3049f50ee068a7e9
Dimitris Verginis <d.verginis777@gmail.com>
2022-04-29 16:05:23 +0300

    fixed-git-layout

--- weatherApp/mainApp.py
+++ weatherApp/mainApp.py
@@ -1,24 +1,34 @@
 
+from re import S
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtCore as qtc
 import PyQt5.QtGui as qtg
-from maingui import Ui_Form
+import weatherGUI
 API_KEY = "40532d33f39f395622bd004abeb82179"
 geoKEY = "f7abd3f018df76b5c983da7768b7cf2a"
 default_call = "https://api.openweathermap.org/data/2.5/"
 units = "metric"
 
-class LoginGui(qtw.QWidget):
+class WeatherApp(qtw.QWidget):
 
     def __init__(self):
         super().__init__()
-        self.loginUI = Ui_Form()
-        self.loginUI.setupUi(self)
+        self.GUI = weatherGUI.Ui_Form()
+        self.GUI.setupUi(self)
+        self.GUI.stackedWidget.setCurrentIndex(0)
+
+        self.GUI.searchButton.clicked.connect(self.search)
+        self.GUI.goBackButton.clicked.connect(self.goBack)
         self.show()
 
+    def search(self):
+        self.GUI.stackedWidget.setCurrentIndex(1)
+
+    def goBack(self):
+        self.GUI.stackedWidget.setCurrentIndex(0)
 
 
 if __name__ == '__main__':
     app = qtw.QApplication([])
-    mw = LoginGui()
+    mw = WeatherApp()
     app.exec_()
