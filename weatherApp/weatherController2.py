8b395b5c194699cdad8d950517982d8c28275d8a
Jopapadam <jopapadam@hotmail.co.uk>
2022-04-30 17:55:51 +0300

    weathercontroller2 fixes

--- weatherApp/weatherController2.py
+++ weatherApp/weatherController2.py
@@ -30,17 +30,7 @@ class WeatherController2:
         if data.status_code in range(400,600):
             raise BadRequest(data.status_code)
         datap=data.json()
-        datap=datap["main"]["temp"]
-        return datap
-    def gethumidityCity(self,CityName):
-        apiCALL = self.getDefaultCall() + \
-            f"weather?q={CityName}&" + \
-                f"exclude=minutely,daily,alerts,hourly&units={self.getUnits()}&appid={self.__getAPIKEY()}"    
-        data = requests.get(apiCALL)
-        if data.status_code in range(400,600):
-            raise BadRequest(data.status_code)
-        datap=data.json()
-        datap=datap["main"]["humidity"]
+        datap=datap["main"]
         return datap
 
     
