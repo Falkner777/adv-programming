989529b2f350bce491b7f4c1c90ea9e6e286e8de
Dimitris Verginis <d.verginis777@gmail.com>
2022-07-03 20:56:30 +0300

    updated weatherCLI
    Co-Authored-By: Jopapadam <54153525+Jopapadam@users.noreply.github.com>

--- weatherApp/CLI/WeatherCLI.py
+++ weatherApp/CLI/WeatherCLI.py
@@ -1,16 +1,102 @@
-import argparse
-import sys
+from ast import match_case
+import math
+import os,sys
+
+from pip import main
+path = os.getcwd()
+parentPath = os.path.dirname(path) + "/weatherApp"
+sys.path.insert(0,parentPath)
+from Exceptions.badRequest import BadRequest
+import datetime
+import requests
+from dateutil.tz import tzoffset
+from Controllers.coordController import CoordController
 import keys
-from Controllers.weatherController import WeatherController
 from dataManager import DataManager
+from Controllers.weatherController import WeatherController
 units = "metric"
 API_KEY = keys.API_KEY
-cityName=input()
 WeatherCaller = WeatherController(API_KEY, units)
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers()
-# parser_getweathercity = subparsers.add_parser('getweathercity')
-# parser_getweathercity.set_defaults(func=WeatherCaller.getWeatherCity)
-data = WeatherCaller.getWeatherCity(cityName)
-info = DataManager.returnCurrentInfo(data)
-print(info)
+
+def printBasicData(data,cityname,currentTime):
+    print(f"------------------{cityname}------------------")
+    print(f"----------------{currentTime}------------------")
+    print(f"Description: {data['weather'][0]['description'].title()}")
+    print(f"Temperature:{data['main']['temp']}")
+    print(f"Feels_Like: {data['main']['feels_like']}")
+    print(f"Pressure: {data['main']['pressure']}")
+    print(f"Humidity: {data['main']['humidity']}")
+    print(f"Wind-Speed:{data['wind']['speed']}")
+    print()
+    # print(f"------------------------------------------")
+
+if __name__ == '__main__':
+    while True:
+        print("----------------------------------------------------------------")
+        print("-------------------Welcome to the WeatherApp!-------------------")
+        print("----------------------------------------------------------------")
+        print("Enter your desired location(type 0 for exit):")
+        
+        try:
+            cityName = input()
+            if cityName == '0':
+                exit()
+            data = WeatherCaller.getWeatherCity(cityName)
+            tzinfo = tzoffset(None, data["timezone"])  
+            currTime = datetime.datetime.now(tzinfo).strftime("%H:%M:%S")
+            printBasicData(data,cityName,currTime)
+        except BadRequest:
+            print("You have entered an invalid city name!!\n\n")
+
+        while True:
+            print("Please enter if you would like to get extra information:\n\
+                1.Get hourly data\n\
+                2.Get weekly data\n\
+                3.Get history data\n\
+                4.Search for another location\n\
+                5.Exit")
+
+            choice = input()
+            match choice:
+                case '1':
+                    hourlyData = WeatherCaller.getHourlyData(cityName)
+                    #print("Choose between 1.")
+                    choiceMapper = {"Temperature": "temp", "Feels-Like": "feels_like", "Pressure": "pressure", "Humidity": "humidity",
+                    "Soil Temperature": "dew_point", "Wind speed": "wind_speed", "Cloudiness": "clouds",
+                    "UV": "uvi", "Precipitation": "pop"}
+        
+                    for hour in hourlyData:
+                        dt = datetime.datetime.fromtimestamp(hour["dt"],tz= tzinfo).strftime('%H:%M')
+                        print(f"{dt}-{hour['temp']}")
+                    
+                
+                    continue
+                case '2':
+                    continue
+                case '3':
+                    continue
+                case '4':
+                    break
+                case '5':
+                    exit()
+                case _:
+                    print("\n\nPlease choose an integer value between 1 and 5\n\n")
+                    continue
+
+        # input("\n\nPress Enter to continue...")
+  
+
+
+        # if choice==2:
+        #     print("Insert the name of the city")
+        #     cityName=input()   
+
+        # if choice==3:
+        #     print("Insert the name of the city")
+        #     cityName=input()       
+        #     WeatherCaller = WeatherController(API_KEY, units)
+        #     data = WeatherCaller.getDailyData(cityName)
+        #     today=datetime.date.today()
+        #     for i in range (7):
+        #         print(today + datetime.timedelta(days=i),data[i]['temp'])
+            
