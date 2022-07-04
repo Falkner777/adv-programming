c44fbd5945e66a90f991ac61d74e077c6017f679
Dimitris Verginis <d.verginis777@gmail.com>
2022-07-04 21:54:06 +0300

    minor-fixes
    Co-Authored-By: Jopapadam <54153525+Jopapadam@users.noreply.github.com>

--- weatherApp/CLI/WeatherCLI.py
+++ weatherApp/CLI/WeatherCLI.py
@@ -21,16 +21,32 @@ from Controllers.modelDB import ModelDB,updateCity,updateDailyData,getCityID
 API_KEY = keys.API_KEY
 
 def loadData(dbConnector,cityName):
-        dataQuery = f"SELECT  date, temp_max,humidity,pressure,wind_speed,uvi,clouds,pop from DAY \
-            where area_id == {getCityID(dbConnector,cityName)} LIMIT 50"
+    """
+    It takes a database connector and a city name as input, and returns a list of tuples containing the
+    data for the last 50 days for that city
+    
+    :param dbConnector: The database connector object
+    :param cityName: The name of the city you want to get data for
+    """
+   
+    dataQuery = f"SELECT  date, temp_max,humidity,pressure,wind_speed,uvi,clouds,pop from DAY \
+        where area_id == {getCityID(dbConnector,cityName)} LIMIT 50"
 
-        res = dbConnector.selectData(dataQuery)
-        colNames = ("Date","Temperature","Humidity","Pressure","Wind Speed","UVI","Clouds","Precipitation")
-        print(colNames)
-        for row in res:
-            pprint(row)
+    res = dbConnector.selectData(dataQuery)
+    colNames = ("Date","Temperature","Humidity","Pressure","Wind Speed","UVI","Clouds","Precipitation")
+    print(colNames)
+    for row in res:
+        pprint(row)
 
 def printBasicData(data,cityname,currentTime):
+    """
+    This function takes in the data, city name, and current time and prints out the basic weather data
+    for the city
+    
+    :param data: The data that we got from the API
+    :param cityname: The name of the city you want to get the weather for
+    :param currentTime: The current time in the city
+    """
     print(f"------------------{cityname}------------------")
     print(f"----------------{currentTime}------------------")
     print(f"Description: {data['weather'][0]['description'].title()}")
@@ -43,18 +59,30 @@ def printBasicData(data,cityname,currentTime):
     # print(f"------------------------------------------")
     
 def printhourly(data):
+    """
+    It takes in a string, and prints out the hourly data for that string
+    
+    :param data: The data you want to print
+    """
     for hour in hourlyData:
         dt = datetime.datetime.fromtimestamp(hour["dt"],tz= tzinfo).strftime('%H:%M')
         print(f"{dt}-{hour[choiceMapper[data]]}")
 
 def printdaily(data):
+    """
+    It takes a data type (e.g. "temp") and prints the date and the value of that data type for each day
+    in the forecast
+    
+    :param data: The data you want to print
+    """
     for day in dailyData:
         dt = datetime.datetime.fromtimestamp(day["dt"],tz= tzinfo).strftime('%D')
         print(f"{dt} - {day[DailyMapper[data]]}")        
 
 if __name__ == '__main__':
     
-    dbConnector = ModelDB("./weatherApp/Data/weatherapp.sqlite3")
+    # dbConnector = ModelDB("./weatherApp/Data/weatherapp.sqlite3")
+    dbConnector = ModelDB("./Data/weatherapp.sqlite3")
     coordController = CoordController(API_KEY)
     print(path)
     print("----------------------------------------------------------------")
@@ -104,11 +132,8 @@ if __name__ == '__main__':
                         printhourly(hourlyChoice)
                     else:       
                         print("You have entered an invalid city name!!\n\n")
-        
-                    # for hour in hourlyData:
-                    #     dt = datetime.datetime.fromtimestamp(hour["dt"],tz= tzinfo).strftime('%H:%M')
-                    #     print(f"{dt}-{hour['temp']}")
-                    
+
+               
                     input("Press any key to continue...")
                     continue
                 case '2':
@@ -137,20 +162,3 @@ if __name__ == '__main__':
             
             
 
-        # input("\n\nPress Enter to continue...")
-  
-
-
-        # if choice==2:
-        #     print("Insert the name of the city")
-        #     cityName=input()   
-
-        # if choice==3:
-        #     print("Insert the name of the city")
-        #     cityName=input()       
-        #     WeatherCaller = WeatherController(API_KEY, units)
-        #     data = WeatherCaller.getDailyData(cityName)
-        #     today=datetime.date.today()
-        #     for i in range (7):
-        #         print(today + datetime.timedelta(days=i),data[i]['temp'])
-            
